676bd717a792fe15dcc085a0dca0d3d3489e0c13
Dan Manning <dmanning23@hotmail.com>
2016-05-08 19:11:38 -0500

    fixed typo

--- TouchBuddyExample.SharedProject/Game1.cs
+++ TouchBuddyExample.SharedProject/Game1.cs
@@ -17,39 +17,6 @@ namespace TouchBuddyExample
 		#region Properties
 
 		GraphicsDeviceManager graphics;
-		SpriteBatch spriteBatch;
-
-		IInputHelper input
-		{
-			get; set;
-		}
-
-		Primitive Prim
-		{
-			get; set;
-		}
-
-		List<HighlightEventArgs> ButtonPresses
-		{
-			get; set;
-		}
-
-		List<ClickEventArgs> Clicks
-		{
-			get; set;
-		}
-
-		DragEventArgs Drag
-		{
-			get; set;
-		}
-
-		List<DragEventArgs> Drags
-		{
-			get; set;
-		}
-
-		List<DropEventArgs> Drops { get; set; }
 
 		#endregion //Properties
 
@@ -59,10 +26,6 @@ namespace TouchBuddyExample
 		{
 			graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
-			ButtonPresses = new List<HighlightEventArgs>();
-			Clicks = new List<ClickEventArgs>();
-			Drops = new List<DropEventArgs>();
-			Drags = new List<DragEventArgs>();
 		}
 
 		/// <summary>
@@ -73,29 +36,11 @@ namespace TouchBuddyExample
 		/// </summary>
 		protected override void Initialize()
 		{
-			input = new TouchComponent(this);
+			var input = new TouchComponent(this);
+
+			var debug = new InputHelper.DebugInputComponent(this);
 
 			base.Initialize();
-		}
-
-		/// <summary>
-		/// LoadContent will be called once per game and is the place to load
-		/// all of your content.
-		/// </summary>
-		protected override void LoadContent()
-		{
-			// Create a new SpriteBatch, which can be used to draw textures.
-			spriteBatch = new SpriteBatch(GraphicsDevice);
-			Prim = new Primitive(GraphicsDevice, spriteBatch);
-		}
-
-		/// <summary>
-		/// UnloadContent will be called once per game and is the place to unload
-		/// game-specific content.
-		/// </summary>
-		protected override void UnloadContent()
-		{
-			// TODO: Unload any non ContentManager content here
 		}
 
 		/// <summary>
@@ -110,34 +55,6 @@ namespace TouchBuddyExample
 				//Exit();
 			}
 
-			Drag = null;
-
-			ButtonPresses.AddRange(input.Highlights);
-			Clicks.AddRange(input.Clicks);
-			Drags.AddRange(input.Drags);
-			Drag = input.Drags.FirstOrDefault();
-			Drops.AddRange(input.Drops);
-
-			while (ButtonPresses.Count > 5)
-			{
-				ButtonPresses.RemoveAt(0);
-            }
-
-			while (Clicks.Count > 5)
-			{
-				Clicks.RemoveAt(0);
-			}
-
-			while (Drops.Count > 5)
-			{
-				Drops.RemoveAt(0);
-			}
-
-			while (Drags.Count > 30)
-			{
-				Drags.RemoveAt(0);
-			}
-
 			base.Update(gameTime);
 		}
 
@@ -148,47 +65,6 @@ namespace TouchBuddyExample
 		protected override void Draw(GameTime gameTime)
 		{
 			GraphicsDevice.Clear(Color.CornflowerBlue);
-
-			spriteBatch.Begin();
-
-			//darw the mouse cursor
-			Prim.Thickness = 1;
-			//Prim.Circle(Mouse.MousePos, 5, Color.White);
-
-			//draw the button down events
-			Prim.Thickness = 2;
-
-			foreach (var mouseEvent in ButtonPresses)
-			{
-				Prim.Circle(mouseEvent.Position, 10, Color.Yellow);
-			}
-
-			//draw the mouse delta in lime green
-			foreach (var mouseEvent in Drags)
-			{
-				Prim.Line(mouseEvent.Current, mouseEvent.Current - mouseEvent.Delta, Color.LimeGreen);
-			}
-
-			//draw the drag operatino in green
-			if (null != Drag)
-			{
-				//Prim.Circle(Drag.Current, 10, Color.Green);
-				Prim.Line(Drag.Start, Drag.Current, Color.Green);
-			}
-
-			foreach (var mouseEvent in Clicks)
-			{
-				Prim.Circle(mouseEvent.Position, 10, (mouseEvent.Button == MouseButton.Left) ? Color.Red : Color.DarkRed);
-			}
-
-			//draw the drop in dark green
-			foreach (var mouseEvent in Drops)
-			{
-				Prim.Line(mouseEvent.Start, mouseEvent.Drop, Color.DarkGreen);
-				Prim.Circle(mouseEvent.Drop, 10, Color.DarkGreen);
-			}
-
-			spriteBatch.End();
 
 			base.Draw(gameTime);
 		}
