9005227ac86018e3fa5630959bce329b938a29d5
Dan Manning <dmanning@oceaneering.com>
2020-12-27 12:53:53 -0600

    test ios

--- TouchBuddyExample.SharedProject/Game1.cs
+++ TouchBuddyExample.SharedProject/Game1.cs
@@ -6,7 +6,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Input.Touch;
 using System.Linq;
-using System.Runtime.InteropServices;
 using TouchScreenBuddy;
 
 namespace TouchBuddyExample
